b923ec29538e864bd5d530424ac07b1464273e26
Christopher French <v-chrfr@microsoft.com>
2018-09-05 15:12:21 -0700

    Add project files.

---
+++ GridBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GridBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
